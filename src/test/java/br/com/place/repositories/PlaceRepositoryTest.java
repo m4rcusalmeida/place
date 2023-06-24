dd27686fc46ab7f3e2a6175a53f2c0efdeeaaa02
Marcus Almeida <marcus.almeida@tjpi.jus.br>
2023-06-24 15:00:01 -0300

    test save, findAll and findById in placeRepository

---
+++ src/test/java/br/com/place/repositories/PlaceRepositoryTest.java
@@ -0,0 +1,68 @@
+package br.com.place.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import br.com.place.models.Place;
+
+@DataJpaTest
+class PlaceRepositoryTest {
+
+	@Autowired
+	private PlaceRepository placeRepository;
+
+	@Test
+	@DisplayName("Given Place Object when Save then Return Saved Place")
+	public void testGivenPlaceObject_whenSave_thenReturnSavedPlace() {
+		// Given
+		Place place = Place.builder().name("teste").slug("teste").city("teste").state("teste").build();
+		// When
+		Place savedPlace = placeRepository.save(place);
+		// Then
+		assertNotNull(savedPlace);
+		assertTrue(savedPlace.getId() > 0);
+		assertEquals(place.getName(), savedPlace.getName());
+		assertEquals(place.getSlug(), savedPlace.getSlug());
+		assertEquals(place.getCity(), savedPlace.getCity());
+		assertEquals(place.getState(), savedPlace.getState());
+
+	}
+
+	@Test
+	@DisplayName("Given Place Object when FindById then Return Place Object")
+	public void testGivenPlaceObject_whenFindById_thenReturnPlaceObject() {
+		// Given
+		Place place = Place.builder().name("teste").slug("teste").city("teste").state("teste").build();
+		placeRepository.save(place);
+		// When
+		Place findById = placeRepository.findById(place.getId()).get();
+		// Then
+		assertNotNull(findById);
+		assertEquals(place.getId(), findById.getId());
+
+	}
+
+	@Test
+	@DisplayName("Given Place List when FindAll then Return Place List")
+	public void testGivenPlaceList_whenFindAll_thenReturnPlaceList() {
+		// Given
+		Place place = Place.builder().name("teste").slug("teste").city("teste").state("teste").build();
+		Place place2 = Place.builder().name("teste2").slug("teste2").city("teste2").state("teste2").build();
+		Place savedPlace = placeRepository.save(place);
+		Place savedPlace2 = placeRepository.save(place2);
+		// When
+		List<Place> placeList = placeRepository.findAll();
+		// Then
+		assertNotNull(placeList);
+		assertEquals(2, placeList.size());
+	}
+
+}
